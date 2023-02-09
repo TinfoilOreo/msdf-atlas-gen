4bc05494a883ef3005b12b6c3d8793b22ddba8f2
pr0cr4st1n8r <96307303+TinfoilOreo@users.noreply.github.com>
2023-02-09 17:11:25 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,34 @@
 project "msdf-atlas-gen"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
         
     targetdir "bin/%{cfg.buildcfg}"
     objdir "bin-int/%{cfg.buildcfg}"
+    
+    defines {
+        "_CRT_SECURE_NO_WARNINGS"
+    }
 
     includedirs {
-        "%{IncludeDir.MsdfAtlasGen}",
+        "%{IncludeDir.MsdfGen}",
+        "%{IncludeDir.MsdfAtlasGen}"
     }
         
     files {
-        "src/**.cpp"
+        "msdf-atlas-gen/**.cpp",
+        "msdf-atlas-gen/**.hpp",
+        "msdf-atlas-gen/**.h",
     }
     
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
         
-    filter "system:linux"
-        pic "On"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
         
-        filter "configurations:Debug"
-            runtime "Debug"
-            symbols "On"
-        
-        filter "configurations:Release"
-            runtime "Release"
-            optimize "On"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
